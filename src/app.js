10d7cdd7a3946700e467b24cdfe78547134554b1
Braiton57 <143307681+Braiton57@users.noreply.github.com>
2023-09-29 05:04:48 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const excuseButton = document.getElementById("generateExcuse");
+const excuseText = document.getElementById("excuse");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+excuseButton.addEventListener("click", generateExcuse);
+
+function generateExcuse() {
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+  excuseText.textContent = excuse;
